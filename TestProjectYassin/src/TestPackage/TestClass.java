e4bffd80ac4119001c2f994a88cdfa4c50477431
yelse <yelse@yelsetohy>
2026-03-02 17:07:54 +0200

    Commit 5

--- TestProjectYassin/src/TestPackage/TestClass.java
+++ TestProjectYassin/src/TestPackage/TestClass.java
@@ -2,8 +2,6 @@ package TestPackage;
 public class TestClass {
 	
 	public static void main(String args[]) {
-		System.out.print("Hello World");
-		
-		int num = 5;
+		System.out.print("I love sherif");
 	}
 }
