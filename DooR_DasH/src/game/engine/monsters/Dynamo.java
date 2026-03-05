915c1086a6d04a029459da022a873a6ee5290e6a
yelse <yelse@yelsetohy>
2026-03-05 07:28:07 +0200

    Merge branch 'master' of https://github.com/shikoSH/test.git

---
+++ DooR_DasH/src/game/engine/monsters/Dynamo.java
@@ -0,0 +1,5 @@
+package game.engine.monsters;
+
+public class Dynamo extends Monster{
+	
+}
--- DooR_DasH/src/game/engine/monsters/Monster.java
+++ DooR_DasH/src/game/engine/monsters/Monster.java
@@ -12,7 +12,7 @@ public abstract class Monster implements Comparable{
 	private boolean shielded;
 	private int confusionTurns;
 	
-	Monster(String name, String description, Role originalRole, int energy){
+	public Monster(String name, String description, Role originalRole, int energy){
 		this.name = name;
 		this.description = description;
 		this.originalRole = originalRole;
