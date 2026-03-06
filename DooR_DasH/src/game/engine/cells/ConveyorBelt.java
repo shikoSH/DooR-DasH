6af208cc7da380eb95b06c0b18d84d86f6dea77b
Sherif Sherif <shiko.shwerif@gmail.com>
2026-03-06 11:02:31 +0200

    finished ConveyorBelt class

---
+++ DooR_DasH/src/game/engine/cells/ConveyorBelt.java
@@ -0,0 +1,14 @@
+package game.engine.cells;
+
+public class ConveyorBelt extends TransportCell{
+
+	
+	
+	//child of Transport cell which is a child of cell
+	// TAke care the effect must be positive might change later 
+	public ConveyorBelt(String name, int effect) {
+		super(name, effect);
+
+	}	
+	
+}
