ae406163b9eda95f4189b6be8377a145b6f10a01
ziady <ziady@zinn>
2026-03-06 15:09:34 +0200

    Board

---
+++ DooR_DasH/src/game/engine/Board.java
@@ -0,0 +1,36 @@
+package game.engine;
+import java.util.ArrayList;
+
+import game.engine.cells.Cell;
+import game.engine.cards.Card;
+import game.engine.monsters.Monster;
+
+public class Board {
+	private static Cell[][] boardCells;
+	private static ArrayList<Monster> stationedMonsters;
+	private static ArrayList<Card> originalCards;
+	private static ArrayList<Card> cards;
+	public Board(ArrayList<Card> readCards){ 
+		
+	}
+	
+	public static ArrayList<Monster> getStationedMonsters() {
+		return stationedMonsters;
+	}
+	public static void setStationedMonsters(ArrayList<Monster> stationedMonsters) {
+		Board.stationedMonsters = stationedMonsters;
+	}
+	public static ArrayList<Card> getCards() {
+		return cards;
+	}
+	public static void setCards(ArrayList<Card> cards) {
+		Board.cards = cards;
+	}
+	public static Cell[][] getBoardCells() {
+		return boardCells;
+	}
+	public static ArrayList<Card> getOriginalCards() {
+		return originalCards;
+	}
+	
+}
