838425c299abdaecc0ae31a5a9aa01b4da14c658
sharki <sharki@Ahmed-Walid>
2026-03-06 10:03:25 +0200

    final attributes in constants class

--- DooR_DasH/src/game/engine/Constants.java
+++ DooR_DasH/src/game/engine/Constants.java
@@ -1,19 +1,19 @@
 package game.engine;
 
 public final class Constants {
-public int BOARD_SIZE=100;
-public int BORAD_ROWS=10;
-public int BORAD_COLS=10;
-public int WINNING_POSITION = 99;
-public int STARTING_POSITION = 0;
-public int [] MONSTER_CELL_INDICES= {2, 18, 34, 54, 82, 88};
-public int [] CONVEYOR_CELL_INDICES={6, 22, 44, 52, 66};
-public int []SOCK_CELL_INDICES ={32, 42, 74, 84, 98};
-public int [] CARD_CELL_INDICES={4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
-public int WINNING_ENERGY = 1000;
-public int MIN_ENERGY = 0;
-public int MULTITASKER_BONUS=100;
-public int SCHEMER_STEAL=10;
-public int SLIP_PENALTY = 100;
-public int POWERUP_COST = 500;
+public final int BOARD_SIZE=100;
+public final int BORAD_ROWS=10;
+public final int BORAD_COLS=10;
+public final int WINNING_POSITION = 99;
+public final int STARTING_POSITION = 0;
+public final int [] MONSTER_CELL_INDICES= {2, 18, 34, 54, 82, 88};
+public final int [] CONVEYOR_CELL_INDICES={6, 22, 44, 52, 66};
+public final int []SOCK_CELL_INDICES ={32, 42, 74, 84, 98};
+public final int [] CARD_CELL_INDICES={4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
+public final int WINNING_ENERGY = 1000;
+public final int MIN_ENERGY = 0;
+public final int MULTITASKER_BONUS=100;
+public final int SCHEMER_STEAL=10;
+public final int SLIP_PENALTY = 100;
+public final int POWERUP_COST = 500;
 }
