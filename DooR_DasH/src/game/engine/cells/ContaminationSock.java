11d007878fe472cdadf36c836d5fa86982945d11
ziady <ziady@zinn>
2026-03-06 11:16:19 +0200

    ContaminationSock

---
+++ DooR_DasH/src/game/engine/cells/ContaminationSock.java
@@ -0,0 +1,8 @@
+package game.engine.cells;
+
+public class ContaminationSock extends TransportCell{
+	// the effect value is always negative 
+	ContaminationSock(String name, int effect){ 
+		 super(name,effect);
+	 }
+}
