5760a359a792c72d3e828dfa38b1dfe7b8c20292
Sherif Sherif <shiko.shwerif@gmail.com>
2026-03-06 11:11:41 +0200

    finished ConfusionCard class

---
+++ DooR_DasH/src/game/engine/cards/ConfusionCard.java
@@ -0,0 +1,21 @@
+package game.engine.cards;
+
+	//child of Card
+
+
+public class ConfusionCard extends Card{
+
+	private int duration;
+	
+	
+	public ConfusionCard(String name, String description, int rarity, int duration) {
+		super(name, description, rarity, false);
+		this.duration=duration;
+	}
+	
+	public int getDuration() {
+		return duration;
+	}
+	
+	
+}
