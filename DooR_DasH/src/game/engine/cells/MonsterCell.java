4095c3c4e319c50c14839a9b25be282c1a19f15d
sharki <sharki@Ahmed-Walid>
2026-03-06 10:32:32 +0200

    private atrbts

--- DooR_DasH/src/game/engine/cells/MonsterCell.java
+++ DooR_DasH/src/game/engine/cells/MonsterCell.java
@@ -3,7 +3,7 @@ package game.engine.cells;
 import game.engine.monsters.Monster;
 
 public class MonsterCell extends Cell {
-	 Monster cellMonster;
+	private Monster cellMonster;
 
 	 public Monster getCellMonster() {
 		 return cellMonster;
