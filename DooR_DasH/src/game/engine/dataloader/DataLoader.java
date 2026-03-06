0f51e10c5179a19b1386240f9903ac051d097fbc
Sherif Sherif <shiko.shwerif@gmail.com>
2026-03-06 16:23:21 +0200

    more modifications to Dataloader

--- DooR_DasH/src/game/engine/dataloader/DataLoader.java
+++ DooR_DasH/src/game/engine/dataloader/DataLoader.java
@@ -11,53 +11,31 @@ import java.io.*;
 import java.util.*;
 
 public class DataLoader {
-	private String CARDS_FILE_NAME="cards.csv";
-	private String CELLS_FILE_NAME="cells.csv";
-	private String MONSTERS_FILE_NAME="monsters.csv";
-	
-	
+	private static String CARDS_FILE_NAME="cards.csv";
+	private static String CELLS_FILE_NAME="cells.csv";
+	private static String MONSTERS_FILE_NAME="monsters.csv";
 	
 
-
-/*
-	        try {
-	            BufferedReader br = new BufferedReader(new FileReader("students.csv"));
-	            String line;
-
-	            while ((line = br.readLine()) != null) {
-
-	                String[] values = line.split(",");
-
-	                int id = Integer.parseInt(values[0]);
-	                String name = values[1];
-	                int age = Integer.parseInt(values[2]);
-
-	                Student s = new Student(id, name, age);
-
-	                students.add(s);
-	            }
-
-	            br.close();
-
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-
-	        System.out.println("Students loaded: " + students.size());
-	    }
-*/
 	 public static ArrayList<Card> readCards() throws IOException {
 		 ArrayList<Card> cards = new ArrayList<>();
 		 BufferedReader br = new BufferedReader(new FileReader(CARDS_FILE_NAME));
 		 
 	        String line;
-
+	        
 	        while ((line = br.readLine()) != null) {
-	            System.out.println(line);
+	        	String[] values = line.split(",");
+	        	String name = values[0];
+	        	String description = values[1];
+	            int rarity = Integer.parseInt(values[2]);
+	            boolean lucky = Boolean.parseBoolean(values[3]);
+
+	            Card c = new Card(name,description,rarity,lucky);
+
+	            card.add(s);
 	        }
 
 	        br.close();
-	        cards.add();
+	        return
 		 
 	 }
 	 public static ArrayList<Cell> readCells() throws IOException{}
@@ -67,7 +45,32 @@ public class DataLoader {
 	
 }
 
+/*
+try {
+    BufferedReader br = new BufferedReader(new FileReader("students.csv"));
+    String line;
 
+    while ((line = br.readLine()) != null) {
+
+        String[] values = line.split(",");
+
+        int id = Integer.parseInt(values[0]);
+        String name = values[1];
+        int age = Integer.parseInt(values[2]);
+
+        Student s = new Student(id, name, age);
+
+        students.add(s);
+    }
+
+    br.close();
+
+} catch (Exception e) {
+    e.printStackTrace();
+}
+
+System.out.println("Students loaded: " + students.size());
+*/
 
 /*
 
