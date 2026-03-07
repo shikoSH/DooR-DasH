7bce1277e027aade7d38a6a5c6f4fae444b7f271
sharki <sharki@Ahmed-Walid>
2026-03-07 11:41:31 +0200

    doorcell+canistermodifier

--- DooR_DasH/src/game/engine/cells/DoorCell.java
+++ DooR_DasH/src/game/engine/cells/DoorCell.java
@@ -1,9 +1,10 @@
 package game.engine.cells;
 
 import game.engine.Role;
+import game.engine.interfaces.CanisterModifier;
 
 
-public class DoorCell extends Cell {
+public class DoorCell extends Cell implements CanisterModifier {
 	
 	private Role role;
 	private int energy;
