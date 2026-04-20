1aab834f020b5ed273c169c92209db8956a5e166
sharki <sharki@Ahmed-Walid>
2026-04-20 21:22:34 +0200

    board constructor

--- DooR_DasH/src/game/engine/Board.java
+++ DooR_DasH/src/game/engine/Board.java
@@ -19,6 +19,8 @@ public class Board {
 		stationedMonsters = new ArrayList<Monster>();
 		originalCards = readCards;
 		cards = new ArrayList<Card>();
+		setCardsByRarity();
+		reloadCards();
 	}
 	
 	public Cell[][] getBoardCells() {
