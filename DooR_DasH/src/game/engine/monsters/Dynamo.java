bbbddb23ca9b0a6263b50a482144c8a241739290
ziady <ziady@zinn>
2026-04-21 00:37:01 +0200

    is Frozen

--- DooR_DasH/src/game/engine/monsters/Dynamo.java
+++ DooR_DasH/src/game/engine/monsters/Dynamo.java
@@ -8,7 +8,7 @@ public class Dynamo extends Monster {
 		super(name, description, role, energy);
 	}
 	public void executePowerupEffect(Monster opponentMonster) {
-		opponentMonster.isFrozen();
+		opponentMonster.setFrozen(true);
 	}
 	
 	public void setEnergy(int energy) {
