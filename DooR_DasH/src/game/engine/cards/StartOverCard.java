10fdfed10192d3a95ccc8eef5af04d7ffb6f0bc9
sharki <sharki@Ahmed-Walid>
2026-04-20 17:22:05 +0200

    ghk

--- DooR_DasH/src/game/engine/cards/StartOverCard.java
+++ DooR_DasH/src/game/engine/cards/StartOverCard.java
@@ -1,9 +1,21 @@
 package game.engine.cards;
 
+import game.engine.monsters.Monster;
+
 public class StartOverCard extends Card {
 
 	public StartOverCard(String name, String description, int rarity, boolean lucky) {
 		super(name, description, rarity, lucky);
 	}
-
+	@Override
+	public void performAction(Monster player, Monster opponent) {
+		if(this.isLucky()==true ) {
+			opponent.setPosition(0);
+		}
+		else {
+			player.setPosition(0);
+		}
+		
+		
+	}
 }
