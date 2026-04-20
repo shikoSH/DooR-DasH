1c5e420bfa70f834d4bea0ed15329465a55c8eb6
sharki <sharki@Ahmed-Walid>
2026-04-20 22:12:32 +0200

    game class

--- DooR_DasH/src/game/engine/Game.java
+++ DooR_DasH/src/game/engine/Game.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import game.engine.dataloader.DataLoader;
+import game.engine.exceptions.InvalidMoveException;
+import game.engine.exceptions.OutOfEnergyException;
 import game.engine.monsters.*;
 
 public class Game {
@@ -18,10 +20,12 @@ public class Game {
 		this.board = new Board(DataLoader.readCards());
 		
 		this.allMonsters = DataLoader.readMonsters();
-		
+	
 		this.player = selectRandomMonsterByRole(playerRole);
 		this.opponent = selectRandomMonsterByRole(playerRole == Role.SCARER ? Role.LAUGHER : Role.SCARER);
 		this.current = player;
+		board.setStationedMonsters(allMonsters);
+		board.initializeBoard(DataLoader.readCells());
 	}
 	
 	public Board getBoard() {
@@ -55,5 +59,31 @@ public class Game {
 	    		.findFirst()
 	    		.orElse(null);
 	}
-	
+	 private Monster getCurrentOpponent() {
+		 if(this.getCurrent()==this.getOpponent()) {return this.getPlayer();}
+		 else return this.getOpponent();
+	 }
+	 private int rollDice() {int x= (int) ((Math.random()*6)+1);
+	 return x;}
+	 public void usePowerup() throws OutOfEnergyException{
+		 if (this.getCurrent().getEnergy()>=Constants.POWERUP_COST) {this.getCurrent().executePowerupEffect(getCurrentOpponent());}
+		 else {OutOfEnergyException e= new OutOfEnergyException();
+		 throw e;}
+	 }
+	 public void playTurn() throws InvalidMoveException{
+		 if(this.getCurrent().isFrozen()==true) {this.getCurrent().setFrozen(false);this.switchTurn();}
+		 else {}
+	 }
+	 private void switchTurn() {
+		 setCurrent(this.getCurrentOpponent());
+	 }
+	 private boolean checkWinCondition(Monster monster) {
+		 if (this.getCurrent().getPosition()==99 && this.getCurrent().getEnergy()>=1000) {return true;}
+		 else return false;
+			 } 
+	 public Monster getWinner() {
+		 if(this.checkWinCondition(getPlayer())) {return getPlayer();}
+		 else if (this.checkWinCondition(getOpponent())) {return getOpponent();}
+		 else return null;
+	 }
 }
