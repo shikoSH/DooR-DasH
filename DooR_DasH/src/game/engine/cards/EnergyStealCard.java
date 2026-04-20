d04e95f6ea85be458ff7f66069a2424d3f6d5a69
sharki <sharki@Ahmed-Walid>
2026-04-20 17:20:24 +0200

    asrg

--- DooR_DasH/src/game/engine/cards/EnergyStealCard.java
+++ DooR_DasH/src/game/engine/cards/EnergyStealCard.java
@@ -1,6 +1,7 @@
 package game.engine.cards;
 
 import game.engine.interfaces.CanisterModifier;
+import game.engine.monsters.Monster;
 
 public class EnergyStealCard extends Card implements CanisterModifier {
 	private int energy;
@@ -13,5 +14,29 @@ public class EnergyStealCard extends Card implements CanisterModifier {
 	public int getEnergy() {
 		return energy;
 	}
-	
+	@Override
+	public void performAction(Monster player, Monster opponent) {
+		
+		//initialising variables
+		int player_original_energy = player.getEnergy();
+		int opponent_original_energy = opponent.getEnergy();
+		int change_in_energy = this.getEnergy();
+		
+		//case that opponent energy is greater than the change in energy 
+		if(opponent_original_energy>=change_in_energy) {
+			modifyCanisterEnergy(player,change_in_energy);
+			modifyCanisterEnergy(opponent,-change_in_energy);
+		}
+		else {
+			//case that the opponent energy is less than the change in energy
+			modifyCanisterEnergy(player,opponent.getEnergy());
+			modifyCanisterEnergy(opponent,0);
+		}
+}
+
+	@Override
+	public void modifyCanisterEnergy(Monster monster, int canisterValue) {
+		monster.alterEnergy(canisterValue);
+		
+	}
 }
--- DooR_DasH/src/game/engine/cards/ShieldCard.java
+++ DooR_DasH/src/game/engine/cards/ShieldCard.java
@@ -1,9 +1,34 @@
 package game.engine.cards;
 
+import java.util.ArrayList;
+
+import game.engine.Board;
+import game.engine.monsters.Monster;
+
 public class ShieldCard extends Card {
 	
 	public ShieldCard(String name, String description, int rarity) {
 		super(name, description, rarity, true); 
 	}
+	@Override
+	public void performAction(Monster player, Monster opponent) {
+		//setting the shield to player and removing from the opponent
+		player.setShielded(true);
+		if(opponent.isShielded()== true) {
+			opponent.setShielded(false);
+		}
+		ArrayList<Monster> stationedMonsters = Board.getStationedMonsters();
+		for (int i = 0; i < stationedMonsters.size(); i++) {
+		    Monster temp = stationedMonsters.get(i);
+		    if(temp.getRole()==player.getRole())
+		    {
+		    	temp.setShielded(true);
+		    }
+		    else 
+		    {
+		    	temp.setShielded(false);
+		    }
+		}
 
+	}
 }
