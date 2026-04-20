26a279f324f47f6d6898bda53379f3e4249f629a
sharki <sharki@Ahmed-Walid>
2026-04-20 17:30:15 +0200

    jk

--- DooR_DasH/src/game/engine/cells/ConveyorBelt.java
+++ DooR_DasH/src/game/engine/cells/ConveyorBelt.java
@@ -1,9 +1,20 @@
 package game.engine.cells;
 
+import game.engine.monsters.Monster;
+
 public class ConveyorBelt extends TransportCell {
 
 	public ConveyorBelt(String name, int effect) {
 		super(name, effect);
 	}
-
+	public void transport(Monster monster) {
+		int newPos = monster.getPosition() + Math.abs(super.getEffect());
+	    monster.setPosition(newPos);
+	}
+	
+	public void onLand(Monster landingMonster, Monster opponentMonster) {
+		super.onLand(landingMonster, opponentMonster);
+		
+		this.transport(landingMonster);
+	}
 }
