80fff6e8bbe72460f3500758ff4eacae4656966e
sharki <sharki@Ahmed-Walid>
2026-04-20 17:35:51 +0200

    sr

--- DooR_DasH/src/game/engine/monsters/Dynamo.java
+++ DooR_DasH/src/game/engine/monsters/Dynamo.java
@@ -7,5 +7,12 @@ public class Dynamo extends Monster {
 	public Dynamo(String name, String description, Role role, int energy) {
 		super(name, description, role, energy);
 	}
+	public void executePowerupEffect(Monster opponentMonster) {
+		
+	}
 	
+	public void setEnergy(int energy) {
+		int newEnergy = this.getEnergy()+ 2*energy;
+		super.setEnergy(newEnergy);
+	}
 }
